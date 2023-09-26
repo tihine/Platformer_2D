0ca93a08c27e3b1d8e5bd8505fc614259d9cba4d
KuroTenshi <margot.pellegrin@gmail.com>
2023-09-26 12:37:01 +0200

    avancement

---
+++ Assets/Scripts/CollisionManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CollisionManager : MonoBehaviour
+{
+    Vector3 position;
+    Vector3 vitesse; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        position = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
