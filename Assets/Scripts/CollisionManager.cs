d5946fed7264961aaa79d9717e5905fa127a3b6c
KuroTenshi <margot.pellegrin@gmail.com>
2023-10-03 14:44:02 +0200

    Ajout de collisions a testet

--- Assets/Scripts/CollisionManager.cs
+++ Assets/Scripts/CollisionManager.cs
@@ -8,15 +8,15 @@ public class CollisionManager : MonoBehaviour
 {
     Vector3 position;
     Vector3 vitesse;
-    [SerializedField]
+    [SerializeField]
     GameObject Player;
-    
-    Vector3 PhantomPosition
+
+    Vector3 PhantomPosition;
     // Start is called before the first frame update
     void Start()
     {
         position = transform.position;
-        Phantomposition = position;
+        PhantomPosition = position;
         this.transform.position = position;
     }
 
@@ -25,11 +25,15 @@ public class CollisionManager : MonoBehaviour
     {
         position = transform.position;
         PhantomPosition = position + vitesse;
-        PhantomCube.transform.position = PhantomPosition;
+        this.transform.position = PhantomPosition;
     }
 
-    void OnCollisionEnter()
+    private void OnCollisionEnter(Collision collision)
     {
-
+        if (collision.gameObject != Player)
+        {
+            Vector3 OnGroundPosition = new Vector3(PhantomPosition.x, collision.gameObject.transform.position.y + collision.gameObject.GetComponent<Renderer>().bounds.size.y / 2 + Player.gameObject.GetComponent<Renderer>().bounds.size.y / 2, 0);
+            Player.transform.position = OnGroundPosition;
+        }
     }
 }
