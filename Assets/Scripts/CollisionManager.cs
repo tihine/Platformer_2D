b590d52f8e6c554f35d8a32813edade5e91a93f4
KuroTenshi <margot.pellegrin@gmail.com>
2023-10-03 14:11:28 +0200

    Modification collision + prefab phantom

--- Assets/Scripts/CollisionManager.cs
+++ Assets/Scripts/CollisionManager.cs
@@ -1,21 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class CollisionManager : MonoBehaviour
 {
     Vector3 position;
-    Vector3 vitesse; 
+    Vector3 vitesse;
+    [SerializedField]
+    GameObject Player;
+    
+    Vector3 PhantomPosition
     // Start is called before the first frame update
     void Start()
     {
         position = transform.position;
+        Phantomposition = position;
+        this.transform.position = position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        position = transform.position;
+        PhantomPosition = position + vitesse;
+        PhantomCube.transform.position = PhantomPosition;
+    }
+
+    void OnCollisionEnter()
+    {
+
     }
 }
