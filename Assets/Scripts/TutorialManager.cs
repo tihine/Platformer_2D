8062482a85c65d395e1708ec5e1aca9de9183a31
tihine <victorine.casse@telecom-sudparis.eu>
2023-10-30 10:16:11 +0100

    modification of the tutorial manager

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -16,36 +16,34 @@ public class TutorialManager: MonoBehaviour
     {
         jumpScript = Player.GetComponent<Jump>();
         playerMovesScript = Player.GetComponent<PlayerMoves>();
+        texts[textsIndex].SetActive(true);
     }
     void Update()
     {
-        for(int i = 0; i < texts.Length; i++)
-        {
-            if (i == textsIndex)
-            {
-                texts[textsIndex].SetActive(true);
-            } else {
-                texts[textsIndex].SetActive(false);
-            }
-        }
-
+       
         if (textsIndex == 0)
         {
             if(playerMovesScript.GetVitesseX()!=0)
             {
                 textsIndex++;
+                texts[textsIndex-1].SetActive(false);
+                texts[textsIndex].SetActive(true);
             }
         } else if (textsIndex == 1)
         {
             if(jumpScript.GetVitesseY()!=0)
             {
                 textsIndex++;
+                texts[textsIndex - 1].SetActive(false);
+                texts[textsIndex].SetActive(true);
             }
         }else if (textsIndex == 2)
         {
             if(playerMovesScript.GetIsSprinting())
             {
                 textsIndex++;
+                texts[textsIndex - 1].SetActive(false);
+                texts[textsIndex].SetActive(true);
             }
         }
     }
