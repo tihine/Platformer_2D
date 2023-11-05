939fb9656498d9c1d32619c839145f8fb2014fdb
Isunia14 <94149685+Isunia14@users.noreply.github.com>
2023-11-05 22:23:46 +0100

    Fix UI not working with controller

--- Assets/Scripts/UI/GestionnaireScene.cs
+++ Assets/Scripts/UI/GestionnaireScene.cs
@@ -1,10 +1,21 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.SceneManagement;
 
 public class GestionnaireScene : MonoBehaviour
 {
+ private Focus_UI FocusUIScript;
+ private UnityEvent closeAdditiveScene = new UnityEvent();
+
+ private void Start()
+ {
+  closeAdditiveScene.AddListener(() => FocusUIScript.CallFocus());
+  FocusUIScript = Focus_UI.Instance;
+ }
+
  public void ChangeScene(string sceneName)
  {
   SceneManager.LoadScene(sceneName);
@@ -19,6 +30,7 @@ public class GestionnaireScene : MonoBehaviour
  {
   SceneManager.UnloadSceneAsync(sceneName);
   SoundSingleton.Instance.PlayClick();
+  closeAdditiveScene.Invoke();
  }
 
  public void Quit()
