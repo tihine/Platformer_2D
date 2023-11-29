41b20112c3c8f45166a416627c46a8b3b1acdcc5
tihine <victorine.casse@telecom-sudparis.eu>
2023-11-29 14:42:23 +0100

    added username for players

--- Assets/Scripts/PlayFabManager.cs
+++ Assets/Scripts/PlayFabManager.cs
@@ -3,12 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using PlayFab;
 using PlayFab.ClientModels;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayFabManager : MonoBehaviour
 {
+    public TMP_InputField usernameInput;
+    public GameObject usernameWindow;
     // Start is called before the first frame update
     void Start()
     {
+        usernameWindow.SetActive(false);
         Login();
     }
 
@@ -16,15 +21,24 @@ public class PlayFabManager : MonoBehaviour
     {
         var request = new LoginWithCustomIDRequest
         {
-            CustomId = SystemInfo.deviceUniqueIdentifier,
-            CreateAccount = true
+            CustomId = "Test",
+            CreateAccount = true,
+            InfoRequestParameters = new GetPlayerCombinedInfoRequestParams
+            {
+                GetPlayerProfile = true
+            }
         };
-        PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError); //anonymous account
+        PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnError); //anonymous account
     }
 
-    void OnSuccess(LoginResult result)
+    void OnLoginSuccess(LoginResult result)
     {
         Debug.Log("Successful login/account create");
+        string name = null;
+        if(result.InfoResultPayload.PlayerProfile!=null)
+            name = result.InfoResultPayload.PlayerProfile.DisplayName;
+        if (name == null)
+            usernameWindow.SetActive(true);
     }
 
     void OnError(PlayFabError error)
@@ -59,7 +73,7 @@ public class PlayFabManager : MonoBehaviour
         var request = new GetLeaderboardRequest
         {
             StatisticName = "PlatformScore",
-            StartPosition = 0,
+            StartPosition = 1,
             MaxResultsCount = 10
         };
         PlayFabClientAPI.GetLeaderboard(request, OnLeaderboardGet, OnError);
@@ -69,7 +83,21 @@ public class PlayFabManager : MonoBehaviour
     {
         foreach(var item in result.Leaderboard)
         {
-            Debug.Log(item.Position + " " + item.PlayFabId + " " + item.StatValue);
+            Debug.Log(item.Position + " " + item.DisplayName + " " + item.StatValue);
         }
+    }
+
+    public void SubmitNameButton()
+    {
+        var request = new UpdateUserTitleDisplayNameRequest
+        {
+            DisplayName = usernameInput.text,
+        };
+        PlayFabClientAPI.UpdateUserTitleDisplayName(request, OnDisplayNameUpdate, OnError);
+    }
+
+    void OnDisplayNameUpdate(UpdateUserTitleDisplayNameResult result)
+    {
+        Debug.Log("Updated display name!");
     }
 }
